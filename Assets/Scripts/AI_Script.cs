fc9249bfce8182b9041d6f672afbef4887485622
Sydewa <95858551+Sydewa@users.noreply.github.com>
2023-01-20 12:15:09 +0100

    20_01_2023

--- Assets/Scripts/AI_Script.cs
+++ Assets/Scripts/AI_Script.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class AI_Script : MonoBehaviour
 {
-    enum State {Patrolling, Chasing, Attack}
+    enum State {Patrolling, Chasing, Attack, Wait}
     
     State currentState;
     private NavMeshAgent agent;
@@ -16,6 +16,10 @@ public class AI_Script : MonoBehaviour
 
     [SerializeField]Transform player;
     [SerializeField]float visionRange;
+    [SerializeField]float attackRange;
+
+    [SerializeField]float waitingTime;
+    float elapsedTime;
 
     void Awake()
     {
@@ -25,7 +29,7 @@ public class AI_Script : MonoBehaviour
     void Start()
     {
         currentState = State.Patrolling;
-        destinationIndex = Random.Range(0, destinationPoints.Length);
+        destinationIndex = 0;
     }
 
     void Update()
@@ -36,11 +40,17 @@ public class AI_Script : MonoBehaviour
                 Patrol();
             break;
 
+            case State.Wait:
+                Waiting();
+            break;
+
             case State.Chasing:
                 Chase();
             break;
 
             case State.Attack:
+                Debug.Log("Attack");
+                currentState = State.Patrolling;
             break;
 
             default:
@@ -58,7 +68,18 @@ public class AI_Script : MonoBehaviour
 
         if(Vector3.Distance(transform.position, destinationPoints[destinationIndex].position) < 1)
         {
-            destinationIndex = Random.Range(0, destinationPoints.Length);
+            if(destinationIndex == (destinationPoints.Length - 1))
+            {
+                destinationIndex = 0;
+                currentState = State.Wait;
+                
+            }
+            else
+            {
+                destinationIndex ++;
+                currentState = State.Wait;
+            }
+            
         }
         
         if(Vector3.Distance(transform.position, player.position) < visionRange)
@@ -74,6 +95,23 @@ public class AI_Script : MonoBehaviour
         {
             currentState = State.Patrolling;
         }
+        if(Vector3.Distance(transform.position, player.position) < attackRange)
+        {
+            currentState = State.Attack;
+        }
+    }
+
+    void Waiting()
+    {
+        
+        elapsedTime += Time.deltaTime;
+        
+
+        if(elapsedTime >= waitingTime)
+        {
+            currentState = State.Patrolling;
+            elapsedTime = 0;
+        }
     }
 
     //--------------
@@ -88,5 +126,7 @@ public class AI_Script : MonoBehaviour
 
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, visionRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
